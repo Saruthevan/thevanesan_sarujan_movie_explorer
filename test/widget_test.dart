f443ae49a7030be2d9f51f4b3a527b5bf5c40e26
Sarujan <saruthevan94@gmail.com>
2026-03-30 15:59:23 +0200

    deleted one comment

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,7 +12,7 @@ import 'package:thevanesan_sarujan_movie_explorer/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
